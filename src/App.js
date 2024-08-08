3401e26d91d7f8313906054c173c03de41ba1836
agaur1 <aditya.gaur@here.com>
2024-08-09 01:42:22 +0530

    MINOR: Integrated bot

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
-import HeroSection from './components/HeroSection';
-import Features from './components/Features';
-import ContactUs from './components/ContactUs';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import HeroSection from "./components/HeroSection";
+import Features from "./components/Features";
+import ContactUs from "./components/ContactUs";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import WatsonChat from "./components/WatsonChat";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <ContactUs />
       </div>
       <Footer />
+      <WatsonChat />
     </div>
   );
 }
